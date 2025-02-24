720dde1ade3a331d3b389f47ec62b96b2081080c
Kevin Nguyen <knguyen@matrixpointesoftware.com>
2025-02-24 10:57:51 -0500

    switches keybinds for root string/file search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,8 +3,11 @@ return {
   keys = {
     { "<leader>fS", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
     { "<leader>fs", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+    { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+    { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
     { "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" } },
     { "<leader>fd", "<cmd>Telescope diagnostics<cr>", { desc = "LSP Diagnostics" } },
+    { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References", nowait = true },
   },
   opts = {
     defaults = {
